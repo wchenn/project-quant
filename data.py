c7d9386c5f6bc692ca57670f60c0d105a1833916
William Chen <wc1321@uw.edu>
2026-07-11 18:07:37 -0500

    added stuff

--- data.py
+++ data.py
@@ -21,11 +21,36 @@ implied_log_return = np.log(close['SPY'].iloc[-1] / close['SPY'].iloc[0])
 print(total_log_return, implied_log_return)
 short_ma = close.rolling(window=20).mean()
 long_ma = close.rolling(window=100).mean()
-print(short_ma.head(5), long_ma.head(1005))
+print(short_ma.head(5), long_ma.head(100))
+'''is recent avg change (last 20 days) greater than long-term avg change? (last 100 days). 
+this is a boolean, T means on this date, 20 day avg is higher than 100 day, false 20 day avg is lower
+or equal to 100 day avg'''
 signal = short_ma > long_ma
+print('SIGNAL HERE')
 print(signal.head(105))
 position = signal.shift(1)
 strategy_returns = position * simple_returns
 # print(strategy_returns.head(10))
 print(strategy_returns.iloc[100:115])
 
+cumulative_returns = (1 + strategy_returns).cumprod()
+buyhold_cumulative = (1 + simple_returns).cumprod()
+'''active trading rule vs. passive holding '''
+print(cumulative_returns.tail())
+print(buyhold_cumulative.tail())
+
+'''sharpe ratio -measure of risk-adjusted return, how much return did we 
+get for each unit of risk we took on? Risk is calculated as the standard deviation of returns.
+or how much return are you getting relative to risk'''
+
+mean_daily_return = strategy_returns.mean()
+std_daily_return = strategy_returns.std()
+sharpe_ratio = (mean_daily_return / std_daily_return) * (252 ** 0.5)
+print("Strategy Sharpe:")
+print(sharpe_ratio)
+
+buyhold_mean = simple_returns.mean()
+buyhold_std = simple_returns.std()
+buyhold_sharpe = (buyhold_mean / buyhold_std) * (252 ** 0.5)
+print("Buy-and-hold Sharpe:")
+print(buyhold_sharpe)
