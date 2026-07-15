b4f988af9cb0f8607e34fb7148f2af932bde9a94
William Chen <wc1321@uw.edu>
2026-07-14 21:51:11 -0500

    went back to wide

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 st.set_page_config(
     page_title="Moving Average Crossover Backtest",
     page_icon="📈",
-    layout="centered"
+    layout="wide"
 )
 st.title("Moving Average Crossover Backtest")
 st.subheader('Intro')
